669fc2b6fc663358c86fc105a87ed3e49af2cc19
ErikaAndre27 <219741158+ErikaAndre27@users.noreply.github.com>
2025-09-15 22:41:42 -0500

    Solución Ejercicio 1

---
+++ TiposDeVariables.js
@@ -0,0 +1,14 @@
+let nombre = "Erika Andrea Gonzalez Ramos";
+let edad = 24;
+let meGustaProgramar = true;
+
+let respuesta;
+if (meGustaProgramar==true){
+    respuesta = "me gusta programar"
+}
+else 
+{
+    respuesta = "no me gusta programar"
+}
+
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y ${respuesta}`);
